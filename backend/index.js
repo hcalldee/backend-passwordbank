cc491fcf4a2c6afb4d15392710403c2549ab4c63
hcalldee <hcalldee@gmail.com>
2022-06-19 23:15:32 +0700

    make it better

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ import cors from 'cors'
 
 const app = express()
 
-const PORT = 8080;
+const PORT =process.env.PORT||8080;
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
